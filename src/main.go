b20be7348fa66b68d3d6fc41f8c7138859ee7249
vntero <shin.antero@gmail.com>
2022-07-15 00:20:01 +0200

    started writing helloHandler func

--- src/main.go
+++ src/main.go
@@ -6,6 +6,10 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
