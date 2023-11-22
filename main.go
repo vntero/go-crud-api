ceb4cb18c2351fe4203c598b28e4f21c2d4497d3
vntero <shin.antero@gmail.com>
2023-11-22 21:58:01 +0100

    binds update ctr to the router

--- main.go
+++ main.go
@@ -13,10 +13,9 @@ func main() {
 	models.ConnectDatabase()
 
 	router.POST("/posts", controllers.CreatePost)
-
 	router.GET("/posts", controllers.FindPosts)
-
 	router.GET("/posts/:id", controllers.FindPost)
+	router.PATCH("posts/:id", controllers.UpdatePost)
 
 	router.Run("localhost:8080")
 }
